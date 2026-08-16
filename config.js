c560a5bf226805483b9e065462afb1e988ceb390
iwasaki2416-cmyk <iwasaki.2416@gmail.com>
2026-08-16 11:20:21 +0900

    Supabaseログイン同期を有効化

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.NIGHT_SHIFT_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://gamfkeykxvroljvplnrz.supabase.co",
+  supabaseAnonKey: "sb_publishable_LZPLDRZY2G9lsmWtFTnA4Q_4_mTvxvU"
 };
